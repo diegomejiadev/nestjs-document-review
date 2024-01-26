116251c1c290f0d4975ce56af9abd369f731b7cc
Diego Mejia <diegorobertomejiasalinas@gmail.com>
2024-01-26 18:44:07 -0500

    chore: Optional field in interface

--- src/modules/applicant/domain/interfaces/applicant.interface.ts
+++ src/modules/applicant/domain/interfaces/applicant.interface.ts
@@ -7,6 +7,6 @@ export interface IApplicant {
   email: string;
   password: string;
   // sentDocuments: IDocument[];
-  createdAt: Date;
-  updatedAt: Date;
+  createdAt?: Date;
+  updatedAt?: Date;
 }
