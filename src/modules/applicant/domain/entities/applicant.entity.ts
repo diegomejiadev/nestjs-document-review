a862100fbf45721729dfb501d97fa621b43480e2
Diego Mejia <diegorobertomejiasalinas@gmail.com>
2024-01-26 18:43:40 -0500

    feat: Added Exclude serializer in Applicant Entity

--- src/modules/applicant/domain/entities/applicant.entity.ts
+++ src/modules/applicant/domain/entities/applicant.entity.ts
@@ -1,14 +1,17 @@
-import { IDocument } from 'src/modules/document/domain/interfaces/document.interface';
 import { IApplicant } from '../interfaces/applicant.interface';
 import { DocumentEntity } from 'src/modules/document/domain/entities/document.entity';
+import { Exclude } from 'class-transformer';
 
 export class ApplicantEntity implements IApplicant {
   id: string;
   name: string;
   lastname: string;
   email: string;
+
+  @Exclude()
   password: string;
-  sentDocuments: DocumentEntity[];
-  createdAt: Date;
-  updatedAt: Date;
+
+  sentDocuments?: DocumentEntity[];
+  createdAt?: Date;
+  updatedAt?: Date;
 }
