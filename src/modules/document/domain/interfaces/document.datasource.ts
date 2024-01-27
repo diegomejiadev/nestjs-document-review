c6cac11e02b313e1e6a94dc96b35b798618330a1
Diego Mejia <diegorobertomejiasalinas@gmail.com>
2024-01-26 21:32:33 -0500

    chore: Added functions to Document Datasource Interface

--- src/modules/document/domain/interfaces/document.datasource.ts
+++ src/modules/document/domain/interfaces/document.datasource.ts
@@ -1,4 +1,6 @@
+import { DOCUMENT_STATUS } from 'src/core/constants/document-status.cst';
 import { CreateDocumentDto } from '../dto/create-document.dto';
+import { UpdateDocumentDto } from '../dto/update-document.dto';
 import { DocumentEntity } from '../entities/document.entity';
 
 export interface IDocumentDatasource {
@@ -7,4 +9,31 @@ export interface IDocumentDatasource {
     fileUrl: string,
     body: CreateDocumentDto,
   ): Promise<DocumentEntity>;
+
+  updateBasicInfo(
+    documentId: string,
+    body: UpdateDocumentDto,
+  ): Promise<DocumentEntity>;
+
+  updateFileUrl(
+    documentId: string,
+    newFileUrl: string,
+  ): Promise<DocumentEntity>;
+
+  updateStatus(
+    documentId: string,
+    status: DOCUMENT_STATUS,
+  ): Promise<DocumentEntity>;
+
+  assignReviewer(
+    documentId: string,
+    reviewerId: string,
+  ): Promise<DocumentEntity>;
+
+  assignApprover(
+    documentId: string,
+    approverId: string,
+  ): Promise<DocumentEntity>;
+
+  delete(documentId: string): Promise<boolean>;
 }
