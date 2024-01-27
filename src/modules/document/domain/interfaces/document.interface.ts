5dae64c7cf855e51141e43e36de51827e972c949
Diego Mejia <diegorobertomejiasalinas@gmail.com>
2024-01-26 21:31:34 -0500

    chore: Minimal change

--- src/modules/document/domain/interfaces/document.interface.ts
+++ src/modules/document/domain/interfaces/document.interface.ts
@@ -1,7 +1,5 @@
-import { IApplicant } from 'src/modules/applicant/domain/interfaces/applicant.interface';
 import { DOCUMENT_STATUS } from 'src/core/constants/document-status.cst';
 import { DOCUMENT_TYPE } from 'src/core/constants/document-type.cst';
-import { IComment } from 'src/modules/comment/domain/interfaces/comment.interface';
 
 export interface IDocument {
   id: string;
