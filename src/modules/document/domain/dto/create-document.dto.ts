a66b40ac3cd1a823bb7fe4acd0e9f5d9c91a3cb3
Diego Mejia <diegorobertomejiasalinas@gmail.com>
2024-01-26 19:55:10 -0500

    feat: Added decorators in Create Document Dto for validators and swagger

--- src/modules/document/domain/dto/create-document.dto.ts
+++ src/modules/document/domain/dto/create-document.dto.ts
@@ -1,9 +1,35 @@
-import { DOCUMENT_STATUS } from 'src/core/constants/document-status.cst';
+import { ApiProperty } from '@nestjs/swagger';
+import {
+  IsEnum,
+  IsNotEmpty,
+  IsString,
+  IsUrl,
+  MaxLength,
+  MinLength,
+} from 'class-validator';
 import { DOCUMENT_TYPE } from 'src/core/constants/document-type.cst';
 
 export class CreateDocumentDto {
-//   applicantId: string;
+  //   applicantId: string;
+
+  @ApiProperty()
+  @IsNotEmpty()
+  @IsString()
+  @IsUrl()
   fileUrl: string;
+
+  @ApiProperty()
+  @IsNotEmpty()
+  @IsString()
+  @MinLength(3)
+  @MaxLength(250)
   title: string;
+
+  @ApiProperty({
+    enum: DOCUMENT_TYPE
+  })
+  @IsNotEmpty()
+  @IsString()
+  @IsEnum(DOCUMENT_TYPE)
   type: DOCUMENT_TYPE;
 }
