ad6611e9ceba583dde57e8211f45e9fbe6273812
Diego Mejia <diegorobertomejiasalinas@gmail.com>
2024-01-26 19:56:17 -0500

    feat: Created Document Controller

--- src/modules/document/presentation/controllers/document.controller.ts
+++ src/modules/document/presentation/controllers/document.controller.ts
@@ -1,4 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
+import { CreateDocumentDto } from '../../domain/dto/create-document.dto';
+import { DocumentService } from '../services/document.service';
 
+@ApiTags('Documento - Document')
 @Controller('document')
-export class DocumentController {}
+export class DocumentController {
+  constructor(private readonly documentService: DocumentService) {}
+
+  @Post()
+  create(@Body() body: CreateDocumentDto) {
+    return this.documentService.create(body);
+  }
+}
