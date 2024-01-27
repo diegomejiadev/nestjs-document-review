83158dcb5871cbe93dad0b1ed8bed3f928bbe411
Diego Mejia <diegorobertomejiasalinas@gmail.com>
2024-01-27 11:59:53 -0500

    chore: Added new status in Document Status

--- src/core/constants/document-status.cst.ts
+++ src/core/constants/document-status.cst.ts
@@ -1,8 +1,9 @@
 export enum DOCUMENT_STATUS {
   PENDING_REVISION = 'pending', //* El aplicante envia el documento y entra a este estado
   REVIEWER_ASSIGNED = 'reviewer-assigned', //* Se asigna a un reseñador
-  REVIEW_APPROVED = 'approved', //* El reseñador lo aprueba
   PENDING_EDITING = 'editing', //* El reseñador indica que hay que modificar
+  REVIEW_APPROVED = 'approved', //* El reseñador lo aprueba
+  APPROVER_ASSIGNED = 'approver-assigned', //* Tras aprobarse por el reseñador, un aprobador va a tomarlo
   RETURNED = 'returned', //* El aprobador lo regresa y el reseñador debe ver los puntos
   COMPLETED = 'completed', //* Se completa el proceso
   REJECTED = 'rejected', //* Se rechaza el documento
