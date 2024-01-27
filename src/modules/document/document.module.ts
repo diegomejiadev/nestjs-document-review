2fba1eef715714e00fca5c12eb672abf5bf45478
Diego Mejia <diegorobertomejiasalinas@gmail.com>
2024-01-27 11:47:03 -0500

    chore: Imported Reviewer module

--- src/modules/document/document.module.ts
+++ src/modules/document/document.module.ts
@@ -12,17 +12,21 @@ import { CreateDocumentUsecase } from './infrastructure/usecases/create-document
 import { UpdateBasicInfoDocumentUsecase } from './infrastructure/usecases/update-basic-info-document.usecase';
 import { UpdateFileUrlDocumentUsecase } from './infrastructure/usecases/update-file-url-document.usecase';
 import { FindDocumentByIdUsecase } from './infrastructure/usecases/find-document-by-id.usecase';
+import { AssignReviewerUsecase } from './infrastructure/usecases/assign-reviewer.usecase';
+import { ReviewerModule } from '../reviewer/reviewer.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([DocumentEntityTypeorm])],
+  imports: [TypeOrmModule.forFeature([DocumentEntityTypeorm]), ReviewerModule],
   controllers: [DocumentController],
   providers: [
     { provide: 'DOCUMENT_REPOSITORY', useClass: DocumentDatasourceTypeorm },
+
     DocumentService,
     CreateDocumentUsecase,
     UpdateBasicInfoDocumentUsecase,
     UpdateFileUrlDocumentUsecase,
     FindDocumentByIdUsecase,
+    AssignReviewerUsecase,
   ],
 })
 export class DocumentModule {}
