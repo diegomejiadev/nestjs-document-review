87b03cada9d1d88304daac3dbf3df8a858fafb8f
Diego Mejia <diegorobertomejiasalinas@gmail.com>
2024-01-26 19:03:14 -0500

    feat: Added swagger

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,23 @@
 import { NestFactory, Reflector } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ClassSerializerInterceptor, ValidationPipe } from '@nestjs/common';
+import { buildSwagger } from './utils/build-swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  //* Creamos un prefix global
+  app.setGlobalPrefix('api');
+
+  //* Agregamos ValidationPipe para class-validator
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
+
+  //* Agregamos ClassSerializerInterceptor para serializar los responses
   app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
+
+  //* Implementamos la funcion para construir swagger
+  buildSwagger(app);
+
   await app.listen(3000);
 }
 bootstrap();
---
+++ src/utils/build-swagger.ts
@@ -0,0 +1,13 @@
+import { INestApplication } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
+export const buildSwagger = (app: INestApplication) => {
+  const config = new DocumentBuilder()
+    .setTitle('Cats example')
+    .setDescription('The cats API description')
+    .setVersion('1.0')
+    .addTag('cats')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
+};
