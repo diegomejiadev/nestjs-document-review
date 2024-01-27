80c270c4f9a515d45ab9a68cb55178322f569f85
Diego Mejia <diegorobertomejiasalinas@gmail.com>
2024-01-26 19:04:03 -0500

    chore: Implemented interface in Applicant module

--- src/modules/applicant/applicant.module.ts
+++ src/modules/applicant/applicant.module.ts
@@ -6,10 +6,20 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ApplicantEntityTypeorm } from './infrastructure/entities/applicant.entity.typeorm';
 import { ApplicantController } from './presentation/controllers/applicant.controller';
+import { ApplicantService } from './presentation/services/applicant.service';
+import { CreateApplicantUsecase } from './infrastructure/usecases/create-applicant.usecase';
+import { ApplicantDatasourceTypeorm } from './infrastructure/datasources/applicant.datasource.typeorm';
 
 @Module({
   imports: [TypeOrmModule.forFeature([ApplicantEntityTypeorm])],
   controllers: [ApplicantController],
-  providers: [],
+  providers: [
+    ApplicantService,
+    CreateApplicantUsecase,
+    {
+      provide: 'APPLICANT_REPOSITORY',
+      useClass: ApplicantDatasourceTypeorm,
+    },
+  ],
 })
 export class ApplicantModule {}
