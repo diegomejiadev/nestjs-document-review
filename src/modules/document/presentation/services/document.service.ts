1b8acf7eec2f7a644f88a3cd2969918182c78b97
Diego Mejia <diegorobertomejiasalinas@gmail.com>
2024-01-26 22:02:20 -0500

    chore: Implemented functions in Document service

--- src/modules/document/presentation/services/document.service.ts
+++ src/modules/document/presentation/services/document.service.ts
@@ -7,22 +7,39 @@ import { CreateDocumentUsecase } from '../../infrastructure/usecases/create-docu
 import { CreateDocumentDto } from '../../domain/dto/create-document.dto';
 import { IResponse } from 'src/core/interfaces/response.interface';
 import { DocumentEntity } from '../../domain/entities/document.entity';
+import { UpdateDocumentDto } from '../../domain/dto/update-document.dto';
+import { UpdateBasicInfoDocumentUsecase } from '../../infrastructure/usecases/update-basic-info-document.usecase';
+import { UpdateFileUrlDocumentUsecase } from '../../infrastructure/usecases/update-file-url-document.usecase';
 
 @Injectable()
 export class DocumentService {
-  constructor(private readonly createDocumentUsecase: CreateDocumentUsecase) {}
+  constructor(
+    private readonly createDocumentUsecase: CreateDocumentUsecase,
+    private readonly updateBasicInfoDocumentUsecase: UpdateBasicInfoDocumentUsecase,
+    private readonly updateFileUrlDocumentUsecase: UpdateFileUrlDocumentUsecase,
+  ) {}
 
   async create(body: CreateDocumentDto): Promise<IResponse<DocumentEntity>> {
-    try {
-      const data = await this.createDocumentUsecase.handle(body);
+    const data = await this.createDocumentUsecase.handle(body);
 
-      return { data };
-    } catch (e) {
-      if (e instanceof HttpException) {
-        throw e;
-      }
+    return { data };
+  }
 
-      throw new InternalServerErrorException('Internal server Error');
-    }
+  async updateBasicInfo(
+    documentId: string,
+    body: UpdateDocumentDto,
+  ): Promise<IResponse<DocumentEntity>> {
+    const data = await this.updateBasicInfoDocumentUsecase.handle(
+      documentId,
+      body,
+    );
+
+    return { data };
+  }
+
+  async updateFileUrl(documentId: string): Promise<IResponse<DocumentEntity>> {
+    const data = await this.updateFileUrlDocumentUsecase.handle(documentId);
+
+    return { data };
   }
 }
