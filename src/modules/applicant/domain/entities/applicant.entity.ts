ec5b038c396c8f6f9dfc39a57e8c988d7e082fc8
Diego Mejia <diegorobertomejiasalinas@gmail.com>
2024-01-27 08:47:45 -0500

    chore: Added getters and setters

--- src/modules/applicant/domain/entities/applicant.entity.ts
+++ src/modules/applicant/domain/entities/applicant.entity.ts
@@ -2,88 +2,103 @@ import { DocumentEntity } from 'src/modules/document/domain/entities/document.en
 import { Exclude } from 'class-transformer';
 
 export class ApplicantEntity {
-  id: string;
-  name: string;
-  lastname: string;
-  email: string;
+  constructor() {}
+
+  private id: string;
+  private name: string;
+  private lastname: string;
+  private email: string;
 
   @Exclude()
-  password: string;
+  private password: string;
 
-  sentDocuments?: DocumentEntity[];
-  createdAt?: Date;
-  updatedAt?: Date;
-  deletedAt?: Date;
+  private sentDocuments?: DocumentEntity[];
+  private createdAt?: Date;
+  private updatedAt?: Date;
+  private deletedAt?: Date;
 
   public getPassword(): string {
     return this.password;
   }
 
-  public setPassword(password: string): void {
+  public setPassword(password: string): this {
     this.password = password;
+    return this;
   }
 
   public getSentDocuments(): DocumentEntity[] {
     return this.sentDocuments;
   }
 
-  public setSentDocuments(sentDocuments: DocumentEntity[]): void {
+  public setSentDocuments(sentDocuments: DocumentEntity[]): this {
     this.sentDocuments = sentDocuments;
+    return this;
   }
 
   public getCreatedAt(): Date {
     return this.createdAt;
   }
 
-  public setCreatedAt(createdAt: Date): void {
+  public setCreatedAt(createdAt: Date): this {
     this.createdAt = createdAt;
+    return this;
   }
 
   public getUpdatedAt(): Date {
     return this.updatedAt;
   }
 
-  public setUpdatedAt(updatedAt: Date): void {
+  public setUpdatedAt(updatedAt: Date): this {
     this.updatedAt = updatedAt;
+    return this;
   }
 
   public getDeletedAt(): Date {
     return this.deletedAt;
   }
 
-  public setDeletedAt(deletedAt: Date): void {
+  public setDeletedAt(deletedAt: Date): this {
     this.deletedAt = deletedAt;
+    return this;
   }
 
   public getId(): string {
     return this.id;
   }
 
-  public setId(id: string): void {
+  public setId(id: string): this {
     this.id = id;
+    return this;
   }
 
   public getName(): string {
     return this.name;
   }
 
-  public setName(name: string): void {
+  public setName(name: string): this {
     this.name = name;
+    return this;
   }
 
   public getLastname(): string {
     return this.lastname;
   }
 
-  public setLastname(lastname: string): void {
+  public setLastname(lastname: string): this {
     this.lastname = lastname;
+    return this;
   }
 
   public getEmail(): string {
     return this.email;
   }
 
-  public setEmail(email: string): void {
+  public setEmail(email: string): this {
     this.email = email;
+    return this;
+  }
+
+  public build(): this {
+    return this;
   }
 }
--- src/modules/document/domain/entities/document.entity.ts
+++ src/modules/document/domain/entities/document.entity.ts
@@ -4,16 +4,130 @@ import { DOCUMENT_TYPE } from 'src/core/constants/document-type.cst';
 import { CommentEntity } from 'src/modules/comment/domain/entities/comment.entity';
 
 export class DocumentEntity {
-  id: string;
-  title: string;
-  fileUrl: string;
-  applicant?: ApplicantEntity;
-  applicantId?: string;
-  type: DOCUMENT_TYPE;
-  submissionDate: Date;
-  status: DOCUMENT_STATUS;
-  comments?: CommentEntity[];
-  createdAt?: Date;
-  updatedAt?: Date;
-  deletedAt?: Date;
+  constructor() {}
+
+  private id: string;
+  private title: string;
+  private fileUrl: string;
+  private applicant?: ApplicantEntity;
+  private applicantId?: string;
+  private type: DOCUMENT_TYPE;
+  private submissionDate: Date;
+  private status: DOCUMENT_STATUS;
+  private comments?: CommentEntity[];
+  private createdAt?: Date;
+  private updatedAt?: Date;
+  private deletedAt?: Date;
+
+  public getId(): string {
+    return this.id;
+  }
+
+  public setId(id: string): this {
+    this.id = id;
+    return this;
+  }
+
+  public getTitle(): string {
+    return this.title;
+  }
+
+  public setTitle(title: string): this {
+    this.title = title;
+    return this;
+  }
+
+  public getFileUrl(): string {
+    return this.fileUrl;
+  }
+
+  public setFileUrl(fileUrl: string): this {
+    this.fileUrl = fileUrl;
+    return this;
+  }
+
+  public getApplicant(): ApplicantEntity {
+    return this.applicant;
+  }
+
+  public setApplicant(applicant: ApplicantEntity): this {
+    this.applicant = applicant;
+    return this;
+  }
+
+  public getApplicantId(): string {
+    return this.applicantId;
+  }
+
+  public setApplicantId(applicantId: string): this {
+    this.applicantId = applicantId;
+    return this;
+  }
+
+  public getType(): DOCUMENT_TYPE {
+    return this.type;
+  }
+
+  public setType(type: DOCUMENT_TYPE): this {
+    this.type = type;
+    return this;
+  }
+
+  public getSubmissionDate(): Date {
+    return this.submissionDate;
+  }
+
+  public setSubmissionDate(submissionDate: Date): this {
+    this.submissionDate = submissionDate;
+    return this;
+  }
+
+  public getStatus(): DOCUMENT_STATUS {
+    return this.status;
+  }
+
+  public setStatus(status: DOCUMENT_STATUS): this {
+    this.status = status;
+    return this;
+  }
+
+  public getComments(): CommentEntity[] {
+    return this.comments;
+  }
+
+  public setComments(comments: CommentEntity[]): this {
+    this.comments = comments;
+    return this;
+  }
+
+  public getCreatedAt(): Date {
+    return this.createdAt;
+  }
+
+  public setCreatedAt(createdAt: Date): this {
+    this.createdAt = createdAt;
+    return this;
+  }
+
+  public getUpdatedAt(): Date {
+    return this.updatedAt;
+  }
+
+  public setUpdatedAt(updatedAt: Date): this {
+    this.updatedAt = updatedAt;
+    return this;
+  }
+
+  public getDeletedAt(): Date {
+    return this.deletedAt;
+  }
+
+  public setDeletedAt(deletedAt: Date): this {
+    this.deletedAt = deletedAt;
+    return this;
+  }
+
+  public build(): this {
+    return this;
+  }
 }
