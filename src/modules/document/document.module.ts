6f303a7a170fbe15abcef6758493102258b0da49
Diego Mejia <diegorobertomejiasalinas@gmail.com>
2024-01-26 19:55:46 -0500

    feat: Injecting class and added providers in Document Module

--- src/modules/document/document.module.ts
+++ src/modules/document/document.module.ts
@@ -6,10 +6,17 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { DocumentEntityTypeorm } from './infrastructure/entities/document.entity.typeorm';
 import { DocumentController } from './presentation/controllers/document.controller';
+import { DocumentService } from './presentation/services/document.service';
+import { DocumentDatasourceTypeorm } from './infrastructure/datasources/document.datasource.typeorm';
+import { CreateDocumentUsecase } from './infrastructure/usecases/create-document.usecase';
 
 @Module({
   imports: [TypeOrmModule.forFeature([DocumentEntityTypeorm])],
   controllers: [DocumentController],
-  providers: [],
+  providers: [
+    { provide: 'DOCUMENT_REPOSITORY', useClass: DocumentDatasourceTypeorm },
+    DocumentService,
+    CreateDocumentUsecase,
+  ],
 })
 export class DocumentModule {}
