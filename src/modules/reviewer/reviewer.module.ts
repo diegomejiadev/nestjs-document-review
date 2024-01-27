15a85f0aaec195095a745328e842fb9a4107dc38
Diego Mejia <diegorobertomejiasalinas@gmail.com>
2024-01-27 10:00:06 -0500

    chore: Created Reviewer service and imported in reviewer module

--- src/modules/reviewer/reviewer.module.ts
+++ src/modules/reviewer/reviewer.module.ts
@@ -6,10 +6,20 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ReviewerEntityTypeorm } from './infrastructure/entities/reviewer.entity.typeorm';
 import { ReviewerController } from './presentation/controllers/reviewer.controller';
+import { ReviewerService } from './presentation/services/reviewer.service';
+import { CreateReviewerUsecase } from './infrastructure/usecases/create-reviewer.usecase';
+import { ReviewerDatasourceTypeorm } from './infrastructure/datasources/reviewer.datasource.typeorm';
 
 @Module({
   imports: [TypeOrmModule.forFeature([ReviewerEntityTypeorm])],
   controllers: [ReviewerController],
-  providers: [],
+  providers: [
+    {
+      provide: 'REVIEWER_REPOSITORY',
+      useClass: ReviewerDatasourceTypeorm,
+    },
+    ReviewerService,
+    CreateReviewerUsecase,
+  ],
 })
 export class ReviewerModule {}
