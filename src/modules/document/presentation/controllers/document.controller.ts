cd08951dd7abed701c8c51cb967e730edce82075
Diego Mejia <diegorobertomejiasalinas@gmail.com>
2024-01-27 20:01:54 -0500

    feat: Added proceed review and approve document into controllers

--- src/modules/document/presentation/controllers/document.controller.ts
+++ src/modules/document/presentation/controllers/document.controller.ts
@@ -3,6 +3,7 @@ import { ApiTags } from '@nestjs/swagger';
 import { CreateDocumentDto } from '../../domain/dto/create-document.dto';
 import { DocumentService } from '../services/document.service';
 import { UpdateDocumentDto } from '../../domain/dto/update-document.dto';
+import { ApproveDocumentDto } from '../../domain/dto/approve-document.dto';
 
 @ApiTags('Documento - Document')
 @Controller('document')
@@ -32,8 +33,24 @@ export class DocumentController {
     return this.documentService.updateFileUrl(documentId);
   }
 
-  @Patch('/assign-reviewer/:id')
+  @Patch('assign-reviewer/:id')
   assignReviewer(@Param('id') documentId: string) {
     return this.documentService.assignReviewer(documentId);
+  }
+
+  @Patch('proceeed-review/:id')
+  proceeedReview(
+    @Param('id') documentId: string,
+    @Body() body: ApproveDocumentDto,
+  ) {
+    return this.documentService.proceedReview(documentId, body);
+  }
+
+  @Patch('approve-document/:id')
+  approveDocument(
+    @Param('id') documentId: string,
+    @Body() body: ApproveDocumentDto,
+  ) {
+    return this.documentService.approveDocument(documentId, body);
   }
 }
