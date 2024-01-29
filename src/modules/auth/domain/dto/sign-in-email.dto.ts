a8a0b392d775bbe05d24e6b2fcc178e75dde512c
Diego Mejia <diegorobertomejiasalinas@gmail.com>
2024-01-29 13:30:03 -0500

    docs: Added swagger decorators in DTO and controller

--- src/modules/auth/domain/dto/sign-in-email.dto.ts
+++ src/modules/auth/domain/dto/sign-in-email.dto.ts
@@ -1,10 +1,13 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsNotEmpty, IsString } from 'class-validator';
 
 export class SignInEmailDto {
+  @ApiProperty()
   @IsNotEmpty()
   @IsString()
   email: string;
 
+  @ApiProperty()
   @IsNotEmpty()
   @IsString()
   password: string;
--- src/modules/auth/presentation/controllers/auth.controller.ts
+++ src/modules/auth/presentation/controllers/auth.controller.ts
@@ -3,6 +3,7 @@ import { SignInEmailDto } from '../../domain/dto/sign-in-email.dto';
 import { AuthService } from '../services/auth.service';
 import { ApiTags } from '@nestjs/swagger';
 
+@ApiTags('Autenticación - Auth')
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
