463616441505664021b0947993e2a86e146169f9
Diego Mejia <diegorobertomejiasalinas@gmail.com>
2024-01-29 08:16:35 -0500

    chore: Updated Approver entity with getters and setters

--- src/modules/approver/domain/entities/approver.entity.ts
+++ src/modules/approver/domain/entities/approver.entity.ts
@@ -1,12 +1,74 @@
 import { DocumentEntity } from 'src/modules/document/domain/entities/document.entity';
 
 export class ApproverEntity {
-  id: string;
-  name: string;
-  lastname: string;
-  approvedDocuments: DocumentEntity[];
-  rejectedDocuments: DocumentEntity[];
-  comments: Comment[];
-  createdAt: Date;
-  updatedAt: Date;
+  private id: string;
+  private name: string;
+  private lastname: string;
+  private assignedDocuments: DocumentEntity[];
+  private comments: Comment[];
+  private createdAt: Date;
+  private updatedAt: Date;
+
+  public getId(): string {
+    return this.id;
+  }
+
+  public setId(id: string): this {
+    this.id = id;
+    return this;
+  }
+
+  public getName(): string {
+    return this.name;
+  }
+
+  public setName(name: string): this {
+    this.name = name;
+    return this;
+  }
+
+  public getLastname(): string {
+    return this.lastname;
+  }
+
+  public setLastname(lastname: string): this {
+    this.lastname = lastname;
+    return this;
+  }
+
+  public getAssignedDocuments(): DocumentEntity[] {
+    return this.assignedDocuments;
+  }
+
+  public setAssignedDocuments(assignedDocuments: DocumentEntity[]): this {
+    this.assignedDocuments = assignedDocuments;
+    return this;
+  }
+
+  public getComments(): Comment[] {
+    return this.comments;
+  }
+
+  public setComments(comments: Comment[]): this {
+    this.comments = comments;
+    return this;
+  }
+
+  public getCreatedAt(): Date {
+    return this.createdAt;
+  }
+
+  public setCreatedAt(createdAt: Date): this {
+    this.createdAt = createdAt;
+    return this;
+  }
+
+  public getUpdatedAt(): Date {
+    return this.updatedAt;
+  }
+
+  public setUpdatedAt(updatedAt: Date): this {
+    this.updatedAt = updatedAt;
+    return this;
+  }
 }
