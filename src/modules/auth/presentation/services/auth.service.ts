ce4d9f9041f7bd586fc4e2509be56e61fa514db4
Diego Mejia <diegorobertomejiasalinas@gmail.com>
2024-01-29 13:29:10 -0500

    chore: Created Auth Service with implementation

--- src/modules/auth/presentation/services/auth.service.ts
+++ src/modules/auth/presentation/services/auth.service.ts
@@ -1,4 +1,14 @@
 import { Injectable } from '@nestjs/common';
+import { SignInApplicantUsecase } from '../../infrastructure/usecases/sign-in-applicant.usecase';
+import { SignInEmailDto } from '../../domain/dto/sign-in-email.dto';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(
+    private readonly signInApplicantUsecase: SignInApplicantUsecase,
+  ) {}
+
+  signInApplicant(body: SignInEmailDto) {
+    return this.signInApplicantUsecase.handle(body);
+  }
+}
