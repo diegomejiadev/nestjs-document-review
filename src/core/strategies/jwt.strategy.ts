0293328676f1d3375c300f2d1df05047722b74f2
Diego Mejia <diegorobertomejiasalinas@gmail.com>
2024-01-29 17:13:37 -0500

    chore: Updated JWT Strategy return payload

--- src/core/strategies/jwt.strategy.ts
+++ src/core/strategies/jwt.strategy.ts
@@ -11,6 +11,6 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
   }
 
   async validate(payload: any) {
-    return { userId: payload.sub, username: payload.username };
+    return { userId: payload.sub, role: payload.role };
   }
 }
