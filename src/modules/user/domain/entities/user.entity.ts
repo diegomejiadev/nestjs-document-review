127c4ba4a50e91c786a5548d650356fab46250bf
Diego Mejia <diegorobertomejiasalinas@gmail.com>
2024-01-29 17:36:52 -0500

    chore: Updated User Entity with getters and setters

--- src/modules/user/domain/entities/user.entity.ts
+++ src/modules/user/domain/entities/user.entity.ts
@@ -1,8 +1,86 @@
+import { Exclude } from 'class-transformer';
+
 export class UserEntity {
-  id: string;
-  name: string;
-  lastname: string;
-  role: string;
-  updatedAt: Date;
-  createdAt: Date;
+  private id: string;
+  private name: string;
+  private lastname: string;
+  private email: string;
+
+  @Exclude()
+  private password: string;
+  private role: string;
+  private updatedAt: Date;
+  private createdAt: Date;
+
+  public getId(): string {
+    return this.id;
+  }
+
+  public setId(id: string): this {
+    this.id = id;
+    return this;
+  }
+
+  public getName(): string {
+    return this.name;
+  }
+
+  public setName(name: string): this {
+    this.name = name;
+    return this;
+  }
+
+  public getLastname(): string {
+    return this.lastname;
+  }
+
+  public setLastname(lastname: string): this {
+    this.lastname = lastname;
+    return this;
+  }
+
+  public getEmail(): string {
+    return this.email;
+  }
+
+  public setEmail(email: string): this {
+    this.email = email;
+    return this;
+  }
+
+  public getPassword(): string {
+    return this.password;
+  }
+
+  public setPassword(password: string): this {
+    this.password = password;
+    return this;
+  }
+
+  public getRole(): string {
+    return this.role;
+  }
+
+  public setRole(role: string): this {
+    this.role = role;
+    return this;
+  }
+
+  public getUpdatedAt(): Date {
+    return this.updatedAt;
+  }
+
+  public setUpdatedAt(updatedAt: Date): this {
+    this.updatedAt = updatedAt;
+    return this;
+  }
+
+  public getCreatedAt(): Date {
+    return this.createdAt;
+  }
+
+  public setCreatedAt(createdAt: Date): this {
+    this.createdAt = createdAt;
+    return this;
+  }
 }
