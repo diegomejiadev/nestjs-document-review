2962a8f6b9de62437d2c5caa18693920927e9a82
Diego Mejia <diegorobertomejiasalinas@gmail.com>
2024-01-29 09:53:02 -0500

    feat: Created Approver Controller implementing service

--- src/modules/approver/approver.module.ts
+++ src/modules/approver/approver.module.ts
@@ -6,10 +6,22 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ApproverEntityTypeorm } from './infrastructure/entities/approver.entity.typeorm';
 import { ApproverController } from './presentation/controllers/approver.controller';
+import { ApproverService } from './presentation/services/approver.service';
+import { CreateApproverUsecase } from './infrastructure/usecases/create-approver.usecase';
+import { FindApproverByIdUsecase } from './infrastructure/usecases/find-approver-by-id.usecase';
+import { ApproverDatasourceTypeorm } from './infrastructure/datasources/approver.datasource.typeorm';
 
 @Module({
   imports: [TypeOrmModule.forFeature([ApproverEntityTypeorm])],
   controllers: [ApproverController],
-  providers: [],
+  providers: [
+    {
+      provide: 'APPROVER_REPOSITORY',
+      useClass: ApproverDatasourceTypeorm,
+    },
+    ApproverService,
+    CreateApproverUsecase,
+    FindApproverByIdUsecase,
+  ],
 })
 export class ApproverModule {}
--- src/modules/approver/presentation/controllers/approver.controller.ts
+++ src/modules/approver/presentation/controllers/approver.controller.ts
@@ -1,4 +1,20 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { ApproverService } from '../services/approver.service';
+import { CreateApproverDto } from '../../domain/dto/create-approver.dto';
+import { ApiTags } from '@nestjs/swagger';
 
+@ApiTags('Aprobador - Approver')
 @Controller('approver')
-export class ApproverController {}
+export class ApproverController {
+  constructor(private readonly approverService: ApproverService) {}
+
+  @Post()
+  create(@Body() body: CreateApproverDto) {
+    return this.approverService.createApprover(body);
+  }
+
+  @Get(':id')
+  findById(@Param('id') approverId: string) {
+    return this.approverService.findApproverById(approverId);
+  }
+}
