011cd9ec5981189ddf7e599b39eb474a3ed283a9
Diego Mejia <diegorobertomejiasalinas@gmail.com>
2024-01-29 13:38:03 -0500

    fix: Fixing import with Config Service for JWT Secret

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -4,16 +4,21 @@ import { AuthService } from './presentation/services/auth.service';
 import { JwtModule } from '@nestjs/jwt';
 import { ApplicantModule } from '../applicant/applicant.module';
 import { SignInApplicantUsecase } from './infrastructure/usecases/sign-in-applicant.usecase';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   controllers: [AuthController],
   providers: [AuthService, SignInApplicantUsecase],
   imports: [
     ApplicantModule,
-    JwtModule.register({
-      global: true,
-      secret: process.env.SECRET_JWT_TOKEN,
-      signOptions: { expiresIn: '10m' },
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        global: true,
+        secret: configService.get('SECRET_JWT_TOKEN'),
+        signOptions: { expiresIn: '10m' },
+      }),
     }),
   ],
 })
