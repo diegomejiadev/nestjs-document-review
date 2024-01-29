62b3997f429230139fdfd2b9008e4c2a3ed17757
Diego Mejia <diegorobertomejiasalinas@gmail.com>
2024-01-29 08:18:56 -0500

    chore: Created Approver Datasoruce interface

---
+++ src/modules/approver/domain/dto/create-approver.dto.ts
@@ -0,0 +1,28 @@
+import {
+  IsNotEmpty,
+  IsString,
+  Matches,
+  MaxLength,
+  MinLength,
+} from 'class-validator';
+
+export class CreateApproverDto {
+  @IsNotEmpty()
+  @IsString()
+  name: string;
+
+  @IsNotEmpty()
+  @IsString()
+  lastname: string;
+
+  @IsNotEmpty()
+  @IsString()
+  @Matches(/^[\w-\.]+@([\w-]+\.)+[\w-]{2,3}$/g)
+  email: string;
+
+  @IsNotEmpty()
+  @IsString()
+  @MinLength(8)
+  @MaxLength(30)
+  password: string;
+}
---
+++ src/modules/approver/domain/interfaces/approver.datasource.ts
@@ -0,0 +1,7 @@
+import { CreateApproverDto } from '../dto/create-approver.dto';
+import { ApproverEntity } from '../entities/approver.entity';
+
+export interface IApproverDatasource {
+  create(body: CreateApproverDto): Promise<ApproverEntity>;
+  findById(approvedId: string): Promise<ApproverEntity | null>;
+}
