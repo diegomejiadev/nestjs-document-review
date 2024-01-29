8895d8e8cab21c9ab8c83c90c492b7becd998cfe
Diego Mejia <diegorobertomejiasalinas@gmail.com>
2024-01-29 14:56:18 -0500

    chore: Created JWT guard

---
+++ src/modules/auth/guards/jwt.guard.ts
@@ -0,0 +1,53 @@
+import {
+  CanActivate,
+  ExecutionContext,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { ConfigService } from '@nestjs/config';
+import { Reflector } from '@nestjs/core';
+import { IS_PUBLIC_KEY } from 'src/core/constants/public.cst';
+
+@Injectable()
+export class JwtGuard implements CanActivate {
+  constructor(
+    private jwtService: JwtService,
+    private configService: ConfigService,
+    private reflector: Reflector,
+  ) {}
+
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    const isPublic = this.reflector.getAllAndOverride<boolean>(IS_PUBLIC_KEY, [
+      context.getHandler(),
+      context.getClass(),
+    ]);
+
+    if (isPublic) return true;
+
+    const request = context.switchToHttp().getRequest();
+    const token = this.extractTokenFromHeader(request);
+
+    if (!token) {
+      throw new UnauthorizedException('Debe ingresar un JWT válido');
+    }
+
+    try {
+      const payload = await this.jwtService.verifyAsync(token, {
+        secret: this.configService.get('SECRET_JWT_TOKEN'),
+        ignoreExpiration: false,
+      });
+
+      request['user'] = payload;
+    } catch (e) {
+      throw new UnauthorizedException('Debe ingresar un JWT válido');
+    }
+
+    return true;
+  }
+
+  private extractTokenFromHeader(request: Request): string | undefined {
+    const [type, token] = request.headers['authorization']?.split(' ') ?? [];
+    return type === 'Bearer' ? token : undefined;
+  }
+}
