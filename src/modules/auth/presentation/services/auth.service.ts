bf8c46a157ee7fdf0b08df454346b3abec2eba18
Diego Mejia <diegorobertomejiasalinas@gmail.com>
2024-01-29 19:34:51 -0500

    chore: Created AuthService file

--- src/modules/auth/presentation/services/auth.service.ts
+++ src/modules/auth/presentation/services/auth.service.ts
@@ -1,14 +1,33 @@
 import { Injectable } from '@nestjs/common';
 import { SignInEmailDto } from '../../domain/dto/sign-in-email.dto';
+import { SignUpReviewerUsecase } from '../../infrastructure/usecases/sign-up-reviewer.usecase';
+import { SignUpEmailDto } from '../../domain/dto/sign-up-email.dto';
+import { SignUpApplicantUsecase } from '../../infrastructure/usecases/sign-up-applicant.usecase';
+import { SignUpApproverUsecase } from '../../infrastructure/usecases/sign-up-approver.usecase';
+import { SignInUsecase } from '../../infrastructure/usecases/sign-in.usecase';
 
 @Injectable()
 export class AuthService {
   constructor(
+    private readonly signUpReviewerUsecase: SignUpReviewerUsecase,
+    private readonly signUpApplicantUsecase: SignUpApplicantUsecase,
+    private readonly signUpApproverUsecase: SignUpApproverUsecase,
+    private readonly signInUsecase: SignInUsecase,
   ) {}
 
-  signInApplicant(body: SignInEmailDto) {
+  signIn(body: SignInEmailDto) {
+    return this.signInUsecase.handle(body);
   }
 
-  signUpApplicant(body: any) {
+  signUpReviewer(body: SignUpEmailDto) {
+    return this.signUpReviewerUsecase.handle(body);
+  }
+
+  signUpApplicant(body: SignUpEmailDto) {
+    return this.signUpApplicantUsecase.handle(body);
+  }
+
+  signUpApprover(body: SignUpEmailDto) {
+    return this.signUpApproverUsecase.handle(body);
   }
 }
