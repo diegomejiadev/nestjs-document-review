52c3221fcdd2e7681eea6ee03c2d3c3d41e669bf
Diego Mejia <diegorobertomejiasalinas@gmail.com>
2024-01-30 00:29:39 -0500

    feat: Increased file size limit for 5mb

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory, Reflector } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ClassSerializerInterceptor, ValidationPipe } from '@nestjs/common';
 import { buildSwagger } from './utils/build-swagger';
+import { json, urlencoded } from 'express';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -17,6 +18,10 @@ async function bootstrap() {
       forbidNonWhitelisted: true,
     }),
   );
+
+  //* Aumentamos el limite
+  app.use(json({ limit: '5mb' }));
+  app.use(urlencoded({ limit: '5mb' }));
 
   //* Agregamos ClassSerializerInterceptor para serializar los responses
   app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
