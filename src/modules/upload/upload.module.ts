3fd683723938ca2ebca4be77be2ba672dd51c0f5
Diego Mejia <diegorobertomejiasalinas@gmail.com>
2024-01-30 00:43:30 -0500

    chore: Added dynamic provider for UploadModule

--- src/modules/upload/upload.module.ts
+++ src/modules/upload/upload.module.ts
@@ -5,6 +5,8 @@ https://docs.nestjs.com/modules
 import { Module } from '@nestjs/common';
 import { UploadRepositoryS3 } from './infrastructure/usecases/upload.repository.s3';
 import { UploadService } from './presentation/services/upload.service';
+import { ConfigService } from '@nestjs/config';
+import { StorageProvider } from 'src/core/config/.env.validation';
 
 @Module({
   imports: [],
@@ -13,13 +15,25 @@ import { UploadService } from './presentation/services/upload.service';
     UploadService,
     {
       provide: 'UPLOAD_REPOSITORY',
-      useClass: UploadRepositoryS3,
+      useFactory: (configService: ConfigService) => {
+        switch (configService.get('STORAGE_PROVIDER')) {
+          case StorageProvider.AWS:
+            return new UploadRepositoryS3(configService);
+
+          default:
+            return new UploadRepositoryS3(configService);
+        }
+      },
+      inject: [ConfigService],
     },
   ],
   exports: [
     {
       provide: 'UPLOAD_REPOSITORY',
-      useClass: UploadRepositoryS3,
+      useFactory: (configService: ConfigService) => {
+        return new UploadRepositoryS3(configService);
+      },
+      inject: [ConfigService],
     },
   ],
 })
